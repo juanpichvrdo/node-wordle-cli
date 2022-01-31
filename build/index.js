9216f645efb11c3a94852cd2d15082c12eba0594
Juan Daniel <jpichardopena@gmail.com>
2022-01-30 22:17:34 -0400

    chore: update build

--- build/index.js
+++ build/index.js
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
 "use strict";
-console.log("Hello world!");
+//  "⬛" "🟨" "🟩"
+const wordleGuess = (guessWord, solutionWord) => {
+    if (solutionWord.length !== 5 || guessWord.length !== 5) {
+        console.log("Word must be 5 letters");
+        return;
+    }
+    const guessLetters = [...guessWord];
+    const solutionLetters = [...solutionWord];
+    const unusedLetters = [...solutionWord];
+    let answer = ["⬛", "⬛", "⬛", "⬛", "⬛"];
+    for (let i = 0; i < solutionLetters.length; i++) {
+        if (solutionLetters[i] === guessLetters[i]) {
+            answer[i] = "🟩";
+        }
+    }
+    console.log(answer);
+};
+let solutionWord = "perps";
+wordleGuess("poppy", solutionWord);
+// wordleGuess("lodge", solutionWord);
+// wordleGuess("fudge", solutionWord);
