f2fad1ce913865548f25a3542c8ed9c709816e78
Juan Daniel <jpichardopena@gmail.com>
2022-01-30 23:08:58 -0400

    feat: get random word from txt file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
 #!/usr/bin/env node
+import fs from "fs";
+import path from "path";
+
+const getWord = (): string => {
+  const wordsPath = path.resolve("src/dict.txt");
+
+  try {
+    const data = fs.readFileSync(wordsPath, "utf8");
+    const wordsArray = data.toString().split("\n");
+    const randomWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+    return randomWord;
+  } catch (e) {
+    // if error getting word make player guess the word 'error'
+    return "error";
+  }
+};
 
 const wordleGuess = (guessWord: string, solutionWord: string) => {
   if (solutionWord.length !== 5 || guessWord.length !== 5) {
@@ -26,11 +42,10 @@ const wordleGuess = (guessWord: string, solutionWord: string) => {
     }
   }
 
+  console.log(solutionWord);
   console.log(answer.join(""));
 };
 
-let solutionWord = "banal";
+let solutionWord = getWord();
 
 wordleGuess("annal", solutionWord);
-// wordleGuess("lodge", solutionWord);
-// wordleGuess("fudge", solutionWord);
